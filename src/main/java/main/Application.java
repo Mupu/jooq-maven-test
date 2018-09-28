76ee6e8b899ae9242acc7c0e50748387bbf8c1c2
Alexander <Mupuk55@web.de>
2018-09-28 11:30:31 +0200

    ...

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -14,10 +14,6 @@ import static org.jooq.generated.Tables.*;
 public class Application {
     public static void main(String[] args) throws Exception {
         System.getProperties().setProperty("org.jooq.no-logo", "true");
-//        System.setProperty("jdbc.user", "mupu");
-//        System.setProperty("jdbc.password", "mupu");
-//        System.setProperty("jdbc.url", "jdbc:mysql://192.168.2.119:3306/meetnow?serverTimezone=UTC");
-//        System.setProperty("jdbc.driver", "com.mysql.cj.jdbc.Driver");
         String user = System.getProperty("jdbc.user");
         String password = System.getProperty("jdbc.password");
         String url = System.getProperty("jdbc.url");
@@ -27,8 +23,8 @@ public class Application {
         Class.forName(driver).newInstance();
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
             DSLContext dslContext = DSL.using(connection, SQLDialect.MYSQL);
-            Result<Record2<String, String>> result = dslContext.select(PERSON.NACHNAME, PERSON.VORNAME).from(PERSON).orderBy(PERSON.VORNAME.asc()).fetch();
 
+            Result<Record2<String, String>> result = dslContext.select(PERSON.NACHNAME, PERSON.VORNAME).from(PERSON).orderBy(PERSON.VORNAME.asc()).fetch();
             for (Record2 r : result) {
                 String firstName = r.getValue(PERSON.VORNAME);
                 String lastName = r.getValue(PERSON.NACHNAME);
